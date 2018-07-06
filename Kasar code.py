fe2ba0f5293ef24972a6af4bb060cded93a3011c
khtut <40304423+khtut@users.noreply.github.com>
2018-07-06 14:55:26 -0400

    Update Kasar code.py

--- Kasar code.py
+++ Kasar code.py
@@ -1,21 +1,44 @@
-########## Kasar data ##########
+###code to create clustered mutation matrix###
 import numpy as np
-import scipy as sp
-import NMFalgorithm as nmf
-from sklearn.decomposition import NMF
-import NMFalgorithm as nmf
-from sklearn.cluster import KMeans
-from sklearn.metrics.pairwise import cosine_similarity
-np.set_printoptions(suppress=True)
+import pandas as pd
 
-V = sp.genfromtxt("kasar2015-cll-mutation-matrix.tsv", delimiter="\t")[1:,1:]
+df = pd.read_csv("kasar2015-wgs-cll-mutations.tsv",sep='\t',header=0)
 
-#using sklearn
-model = NMF(n_components=3,solver='mu')
-W = model.fit_transform(V)
-H = model.components_
+sample = df['Sample']
+cat_idx = df['Category Index']
+cat = sorted(set(df['Category']))
+dist = df['Distance to Nearest Mutation']
+samples = sorted(set(df['Sample']))
+sample_index = dict()
+for i in range(len(df)):
+    for index,name in enumerate(samples):
+        sample_index[name+' - C'] = index*2
+        sample_index[name+' - NC'] = index*2 + 1
+    if dist[i] <= 1000:
+        sample_idx = sample_index[sample[i]+' - C']
+    else:
+        sample_idx = sample_index[sample[i]+' - NC']
 
-#clustering
-kmeans = KMeans(n_clusters=3).fit(H)
-signature = kmeans.cluster_centers_
+M = pd.DataFrame(np.zeros([60,96]),index=sample_index,columns=cat)          
+clustercounts = np.array([np.zeros(96)])
+nonclustercounts = np.array([np.zeros(96)])
 
+for a in range(len(samples)):                                   
+    sample_data = df[df['Sample'] == samples[a]]                
+
+    condition = sample_data['Distance to Nearest Mutation'] <= 1000
+    for b in list(sample_data.index):
+        if condition[b] == True: 
+            idx = cat_idx[b]                                    
+            clustercounts[0,idx] = clustercounts[0,idx] + 1     
+        else: 
+            idx = cat_idx[b]                                            
+            nonclustercounts[0,idx] = nonclustercounts[0,idx] + 1     
+    
+    for c in list(sample_data.index):                           
+        if condition[c] == True:                                
+            M[M.index == samples[a]+' - C'] = list(clustercounts)     
+        else:                                                   
+            M[M.index == samples[a]+' - NC'] = list(nonclustercounts)      
+
+M.to_csv('kasar2015clustermatrix.txt', header=cat, sep='\t', mode='w')
