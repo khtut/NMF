6ec2415b57466aa620697d809716759647ec492e
khtut <40304423+khtut@users.noreply.github.com>
2018-07-16 14:33:22 -0400

    Comparing nsNMF results from R to own results

---
+++ nsnmf.py
@@ -0,0 +1,26 @@
+import numpy as np
+import NMFalgorithm as nmf
+from sklearn.decomposition import NMF
+from sklearn.metrics.pairwise import cosine_similarity
+np.set_printoptions(suppress=True)
+#np.set_printoptions(threshold=np.nan)
+
+V = np.genfromtxt("example-mutation-counts.tsv", delimiter="\t")[1:,1:]
+realsig = np.load("example-signatures.npy")
+
+#using R
+W_r = np.genfromtxt("sampleW.txt", delimiter="\t")[1:,1:]
+H_r = np.genfromtxt("sampleH.txt", delimiter="\t")[1:,1:]
+
+#using own code
+W_own,H_own = nmf.nsnmf(V,5,0.5)
+
+diff = cosine_similarity(np.transpose(W_r),np.transpose(W_own))
+print('Comparing W matrices of R results and own results:\n',diff)
+diff1 = cosine_similarity(H_r,H_own)
+print('Comparing H matrices of R results and own results:\n',diff1)
+
+diff2 = cosine_similarity(H_r,realsig)
+print('Comparing H matrix from R to given signatures:\n',diff2)
+diff3 = cosine_similarity(H_own,realsig)
+print('Comparing H matrix from own code to given signatures:\n',diff3)
